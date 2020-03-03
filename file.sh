d99b4d3e6ef6da93465bb4a3b588e2ce136ca404
JITHIN8765 <61421577+JITHIN8765@users.noreply.github.com>
2020-03-03 15:01:56 +0530

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,10 @@
+echo "username : $(echo $USER)"
+echo "current shell :$SHELL"
+echo "home directory : $HOME"
+echo "o.s.  : $OSTYPE"
+echo "current path : $PATH"
+echo "current working directory : $PWD"
+echo "no. of users logged in :  "
+who -q
+
+
