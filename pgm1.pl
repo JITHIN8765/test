a2ff3f42254087d50e41451d06277a031d725e2c
JITHIN8765 <jithin8765@gmail.com>
2020-03-03 14:13:03 +0530

    intial commit

---
+++ pgm1.pl
@@ -0,0 +1,7 @@
+use strict;
+my $a,$b;
+print "enter two numbers :";
+$a=<stdin>;
+$b=<stdin>;
+my $s=$a+$b;
+print "sum is $s";
---
+++ pgm2.pl
@@ -0,0 +1,12 @@
+use strict;
+my $a;
+print "enter number :";
+$a=<stdin>;
+if(($a%2)==0)
+{
+print "even\n";
+}
+else 
+{
+print "odd\n";
+}
